a654a7ebeb43c8bfe3ebb59084364dd08f59cef0
Bethel Renner <131914606+ziesnp57@users.noreply.github.com>
2024-08-28 12:23:15 +0800

    Update SMCProcessorAMD.hpp

--- SMCProcessorAMD/SMCProcessorAMD.hpp
+++ SMCProcessorAMD/SMCProcessorAMD.hpp
@@ -100,6 +100,10 @@ class SMCProcessorAMD : public IOService {
     static constexpr SMC_KEY KeyPCPR = SMC_MAKE_IDENTIFIER('P','C','P','R');
     static constexpr SMC_KEY KeyPCPT = SMC_MAKE_IDENTIFIER('P','C','P','T');
     static constexpr SMC_KEY KeyPCTR = SMC_MAKE_IDENTIFIER('P','C','T','R');
+
+    // CPU temperature
+	static constexpr SMC_KEY KeyVCxC(size_t i) { return SMC_MAKE_IDENTIFIER('V','C',KeyIndexes[i],'C'); }
+
     static constexpr SMC_KEY KeyTCxD(size_t i) { return SMC_MAKE_IDENTIFIER('T','C',KeyIndexes[i],'D'); }
     static constexpr SMC_KEY KeyTCxP(size_t i) { return SMC_MAKE_IDENTIFIER('T','C',KeyIndexes[i],'P'); }
     static constexpr SMC_KEY KeyTCDD = SMC_MAKE_IDENTIFIER('T','C','D','D');
